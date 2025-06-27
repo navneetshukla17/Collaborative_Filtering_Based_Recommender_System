b289afcdf04784486b252193320d2f76372b238b
Tushar Shukla <tusharshukla@Tushars-MacBook-Air.local>
2025-06-27 15:18:08 +0530

    modified: requirements.txt and app.py

--- Collaborative_Filtering_Based_Recommender_System/exception/exception_handler.py
+++ Collaborative_Filtering_Based_Recommender_System/exception/exception_handler.py
@@ -8,6 +8,7 @@ class AppException(Exception):
     such as python script file line number along with error message
     With custom exception one can easily spot source of error and provide quick fix.    
     """
+
     def __init__(self, error_message: Exception, error_detail: sys):
         """
         :param error_message: error message in string format
@@ -24,6 +25,7 @@ class AppException(Exception):
         _, _, exc_tb = error_detail.exc_info()
         #extracting file name from exception traceback
         file_name = exc_tb.tb_frame.f_code.co_filename 
+
         #preparing error message
         error_message = f"Error occurred python script name [{file_name}]" \
                         f" line number [{exc_tb.tb_lineno}] error message [{error}]."
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sys
+import os
+
 from Collaborative_Filtering_Based_Recommender_System.exception.exception_handler import AppException
-from Collaborative_Filtering_Based_Recommender_System.logger import logging
 
 try:
-    a = 3/0
+    a = 1 / 0
 except Exception as e:
-    logging.info(e)
     raise AppException(e, sys) from e
