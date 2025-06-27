842d86c69dc787ce5b11aa012dfd2cfdc2232ba2
Tushar Shukla <tusharshukla@Tushars-MacBook-Air.local>
2025-06-27 18:50:02 +0530

    added data ingestion

--- app.py
+++ app.py
@@ -2,9 +2,3 @@ import sys
 import os
 from Collaborative_Filtering_Based_Recommender_System.exception.exception_handler import AppException
 from Collaborative_Filtering_Based_Recommender_System.logger.log import logging
-
-try:
-    a = 1 / 0
-except Exception as e:
-    logging.info(e)
-    raise AppException(e, sys) from e
