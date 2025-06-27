f520b5be5168385634d84a25bca4fc9cf192dee4
Tushar Shukla <tusharshukla@Tushars-MacBook-Air.local>
2025-06-27 12:45:14 +0530

    installed required local packages and setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+REPO_NAME = "Collaborative-Filtering-Based-Recommender-System"
+AUTHOR_USER_NAME = "TUSHAR SHUKLA"
+SRC_REPO = "Collaborative-Filtering-Based-Recommender-System"
+LIST_OF_REQUIREMENTS = []
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for Collaborative Filtering Based Recommender System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/navneetshukla17/Collaborative-Filtering-Based-Recommender-System",
+    author_email="tusharshukla707@gmail.com",
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=LIST_OF_REQUIREMENTS
+)
